7f005b0b2884747eb2def6be03049aff6aa1cf65
CyberSupreme3642 <nightshade732@gmail.com>
2024-03-29 12:31:53 -0400

    hello

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js"
 import cars from "./routes/car.js"
-// import customers from "./routes/customer.js"
+import customers from "./routes/customer.js"
 // import jobs from "./routes/job.js"
+// import makes from "./routes/make.js"
+// import models from "./routes/model.js"
 // import services from "./routes/service.js"
-// import technicians from "./routes/technican.js"
-// import visits from "../routes/visit.js"
-// import test from "./routes/test.js"
+// import technicans from "./routes/technican.js"
+import test from "./routes/test.js"
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -15,13 +16,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 // app.use("/record", records);
-// app.use("/test", test);
+//app.use("/test", test);
 app.use("/cars", cars);
-// app.use("/customers", customers);
+app.use("/customers", customers);
 // app.use("/jobs", jobs);
+// app.use("/makes", makes);
+// app.use("/models", models);
 // app.use("/services", services);
-// app.use("/technicians", technicians);
-// app.use("/visit", visits);
+// app.use("/technicans", technicans);
 
 
 app.listen(PORT, () => {
